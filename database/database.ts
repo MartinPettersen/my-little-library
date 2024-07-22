749979219f4bd61755cad88576928b7aab9a5787
MartinPettersen <searchingtheskyfor@gmail.com>
2024-07-22 13:01:01 +0200

    delete irrelevant books

--- database/database.ts
+++ database/database.ts
@@ -4,8 +4,6 @@ import { BookMark } from '../utils/types';
 
 const db = SQLite.openDatabaseSync('bookDatabase.db');
 export const createTables = async () => {
-
-
     await db.execAsync(`
         PRAGMA journal_mode = WAL;
         CREATE TABLE IF NOT EXISTS books (
@@ -15,8 +13,6 @@ export const createTables = async () => {
           read BOOLEAN,
           want BOOLEAN
         )`)
-
-    
 };
 
 
@@ -26,43 +22,54 @@ export const addBook = async (isbn: number) => {
 
 export const getAllBooks = async () => {
     const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books');
-    
+
     return allRows
 }
 
 export const getBook = async (isbn: number) => {
-    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE isbn = $isbn', {$isbn: isbn});    
+    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE isbn = $isbn', { $isbn: isbn });
+    return allRows
+}
+
+export const checkForDeletion = async (id: number) => {
+    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE id = $id AND owned = $owned AND read = $read AND want = $want', {$id: id, $owned: false, $read: false, $want: false });
+    if (allRows.length > 0 ) {
+        deleteBook(allRows[0].id)
+    }
     return allRows
 }
 
 export const getOwnedBooks = async () => {
-    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE owned = $owned', {$owned: true});    
+    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE owned = $owned', { $owned: true });
     return allRows
 }
 export const getReadBooks = async () => {
-    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE read = $read', {$read: true});    
+    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE read = $read', { $read: true });
     return allRows
 }
 
 export const getWishlistedBooks = async () => {
-    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE want = $want', {$want: true});    
+    const allRows: BookMark[] = await db.getAllAsync('SELECT * FROM books WHERE want = $want', { $want: true });
     return allRows
 }
 
-export const deleteBook = async ( id: number) => {
+export const deleteBook = async (id: number) => {
     await db.runAsync('DELETE FROM books WHERE id = $id', { $id: id });
 }
 
-export const updateBookOwnership = async ( id: number, owned: boolean) => {
+export const updateBookOwnership = async (id: number, owned: boolean) => {
     await db.runAsync('UPDATE books SET owned = ? WHERE id = ?', [owned, id])
+    checkForDeletion(id)
 }
 
-export const updateBookRead = async ( id: number, read: boolean) => {
+export const updateBookRead = async (id: number, read: boolean) => {
     await db.runAsync('UPDATE books SET read = ? WHERE id = ?', [read, id])
+    checkForDeletion(id)
 }
 
-export const updateBookWant = async ( id: number,  want: boolean) => {
+export const updateBookWant = async (id: number, want: boolean) => {
     await db.runAsync('UPDATE books SET want = ? WHERE id = ?', [want, id])
+    checkForDeletion(id)
 }
 
 createTables();
